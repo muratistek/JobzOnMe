21730c7519b2794e410ed53f69c0edd831cca6ec
Murat Istek <messi897@gmail.com>
2023-01-28 02:02:25 -0500

    Separate react code and styled components

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import logo from '../assets/images/logo.png'
 import main from '../assets/images/main.png'
-import styled from 'styled-components'
+import Wrapper from '../assets/wrappers/LandingPage'
 
 const Landing = () => {
   return (
@@ -29,52 +29,6 @@ const Landing = () => {
   )
 }
 
-const Wrapper = styled.main`
-  .logo{
-    width: 164px;
-    height: 50px;
-  }
 
-  nav{
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    margin: 0 auto;
-    height: var(--nav-height);
-    display: flex;
-    align-items: center;
-  }
-
-  .page{
-    min-height: calc(100vh - var(--nav-height));
-    display: grid;
-    align-items: center;
-    margin-top: -3rem;
-  }
-
-  h1{
-    font-weight: 700;
-    span{
-      color: var(--primary2-500);
-    }
-  }
-
-  p{
-    color: var(--grey-600);
-  }
-
-  .main-img{
-    display: none;
-  }
-
-  @media (min-width: 992px) {
-    .page{
-      grid-template-columns: 1fr 1fr;
-      column-gap: 3rem;
-    }
-    .main-img{
-      display: block;
-    }
-  }
-`
 
 export default Landing
