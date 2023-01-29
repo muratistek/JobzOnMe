2bd4a7ce09c35f2e055df6b911c62c60fff04da0
Murat Istek <messi897@gmail.com>
2023-01-29 15:35:54 -0500

    Add index.js for pages to increase reusability

---
+++ client/src/pages/index.js
@@ -0,0 +1,8 @@
+import Dashboard from "./Dashboard";
+import Error from "./Error";
+import Landing from "./Landing";
+import Register from "./Register";
+
+export {
+  Dashboard, Register, Landing, Error
+}
