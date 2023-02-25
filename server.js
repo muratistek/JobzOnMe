b89a4c7578913b2a1bd2cdf539320eed14ceecd6
Murat Istek <messi897@gmail.com>
2023-02-25 17:46:10 -0500

    Add Express Server to the project

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("Server running...")
+import express from 'express'
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send("Welcome!")
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => { console.log(`Server is running on port ${port}...`) })
