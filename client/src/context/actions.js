4287b88495c031e9068ceed2e5e10e5fb8aa7db6
Murat Istek <messi897@gmail.com>
2023-02-25 01:44:04 -0500

    Add "Clear Alert" action method

--- client/src/context/actions.js
+++ client/src/context/actions.js
@@ -1,3 +1,4 @@
 // Here we set variables for action names to have a less chance of making small typos
 
 export const DISPLAY_ALERT = 'SHOW_ALERT'
+export const CLEAR_ALERT = 'CLEAR_ALERT'
--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,6 +1,6 @@
 import React, { useContext, useReducer } from "react";
 import reducer from "./reducer";
-import { DISPLAY_ALERT } from "./actions";
+import { CLEAR_ALERT, DISPLAY_ALERT } from "./actions";
 
 const initialState = {
   isLoading: false,
@@ -17,6 +17,14 @@ const AppProvider = ({ children }) => {
 
   const displayAlert = () => {
     dispatch({ type: DISPLAY_ALERT })
+
+    clearAlert()
+  }
+
+  const clearAlert = () => {
+    setTimeout(() => {
+      dispatch({ type: CLEAR_ALERT })
+    }, 3000)
   }
 
   return (
