1c8b32b2aa1cfe37956c075a677a29f3b044947e
Murat Istek <messi897@gmail.com>
2023-02-25 18:25:35 -0500

    Add "errorHandlerMiddleware" to the server

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const app = express()
 
 // Middleware
 import notFoundMiddleware from './middleware/not-found.js'
-notFoundMiddleware
+import errorHandlerMiddleware from './middleware/error-handler.js'
 
 app.get('/', (req, res) => {
   res.send("Welcome!")
 })
 
 app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5005
 
 app.listen(port, () => { console.log(`Server is running on port ${port}...`) })
