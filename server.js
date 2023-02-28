58d1853909f3cd7ba7931aae98c09371cbbd5672
Murat Istek <messi897@gmail.com>
2023-02-27 23:06:24 -0500

    Create auth routes to the server.js

--- server.js
+++ server.js
@@ -4,17 +4,23 @@ const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
 
+// Database
 import connectDB from './db/connect.js'
 
 // Middleware
 import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
 
+// Routers
+import authRouter from './routes/authRoutes.js'
 
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send("Welcome!")
 })
+
+app.use('/api/v1/auth', authRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
