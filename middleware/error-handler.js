90ddde3e98bfc1e3a38599b0086f72ef9397fd74
Murat Istek <messi897@gmail.com>
2023-03-02 17:03:34 -0500

    Add custom error message to the error middleware

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -6,7 +6,19 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
     msg: "There was some error"
   }
-  res.status(defaultError.statusCode).json({ msg: err });
+
+  if (err.name === "ValidationError") {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST
+
+    // More generic message
+    // defaultError.msg = err.message
+
+    // More detailed message
+    defaultError.msg = Object.values(err.errors).map((item) => item.message).join(',')
+  }
+
+  // res.status(defaultError.statusCode).json({ msg: err });
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg })
 }
 
 export default errorHandlerMiddleware
