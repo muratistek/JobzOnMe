2ecdeb876b2093ce0287c7700955e3ea69bd2ad3
Murat Istek <messi897@gmail.com>
2023-03-02 19:38:10 -0500

    Refactor custom error classes

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,25 +2,7 @@ import User from "../models/User.js"
 import { StatusCodes } from "http-status-codes"
 
 // Custom Errors
-class CustomAPIError extends Error {
-  constructor(message) {
-    super(message)
-  }
-}
-
-class BadRequestError extends CustomAPIError {
-  constructor(message) {
-    super(message)
-    this.statusCode = StatusCodes.BAD_REQUEST
-  }
-}
-
-class NotFoundError extends CustomAPIError {
-  constructor(message) {
-    super(message)
-    this.statusCode = StatusCodes.NOT_FOUND
-  }
-}
+import { BadRequestError, NotFoundError } from '../errors/index.js'
 
 const register = async (req, res) => {
   const { name, email, password } = req.body
