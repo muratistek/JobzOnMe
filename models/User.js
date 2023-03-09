9bab7ff9f0b37e9436963554f398dbeb6de531c1
Murat Istek <messi897@gmail.com>
2023-03-09 16:26:58 -0500

    Add hash password functionality

--- models/User.js
+++ models/User.js
@@ -39,8 +39,9 @@ const UserSchema = new mongoose.Schema({
 })
 
 // Setting up the middleware for registering 
-UserSchema.pre('save', function () {
-
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
 })
 
 export default mongoose.model("User", UserSchema)
