3e620ad21e95182801afac9dd8eb4b6b39a9fef2
Murat Istek <messi897@gmail.com>
2023-03-24 17:12:56 -0400

    Add programmatic navigation to dashboard

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Logo, FormRow, Alert } from '../components'
 import Wrapper from '../assets/wrappers/RegisterPage'
 import { useAppContext } from '../context/appContext'
+import { useNavigate } from 'react-router-dom'
 
 const initialState = {
   name: '',
@@ -13,7 +14,8 @@ const initialState = {
 const Register = () => {
   const [values, setValues] = useState(initialState)
   // global state and useNavigate
-  const { isLoading, showAlert, displayAlert, registerUser } = useAppContext()
+  const navigate = useNavigate()
+  const { user, isLoading, showAlert, displayAlert, registerUser } = useAppContext()
 
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember })
@@ -38,6 +40,14 @@ const Register = () => {
       registerUser(currentUser)
     }
   }
+
+  useEffect(() => {
+    if (user) {
+      setTimeout(() => {
+        navigate('/')
+      }, 3000)
+    }
+  }, [user, navigate])
 
   return (
     <Wrapper className='full-page'>
