e89c3450b1ec4a9919c0ad1f1e30be1805ae5af4
Murat Istek <messi897@gmail.com>
2023-04-26 22:25:50 -0400

    Add "react-icons" library

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import Wrapper from "../assets/wrappers/Navbar";
+import { FaHome } from 'react-icons/fa'
 
 export default function Navbar() {
   return (
     <Wrapper>
       <h4>Navbar</h4>
+      <FaHome />
     </Wrapper>
   )
 }
