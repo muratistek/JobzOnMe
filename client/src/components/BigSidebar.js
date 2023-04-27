1d63429fe18085ff88329d7b3ef8d53774e20a0e
Murat Istek <messi897@gmail.com>
2023-04-27 12:50:28 -0400

    Add big sidebar and its links

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/BigSidebar'
+import { useAppContext } from '../context/appContext'
+import Logo from './Logo'
+import NavLinks from './NavLinks'
 
 export default function BigSidebar() {
+  // Potentially we can use "toggleSidebar" if we want the big sidebar to be closed once a user clicks any nav link
+  const { showSidebar } = useAppContext()
+
   return (
     <Wrapper>
-      <h4>Big sidebar</h4>
+      {/* We want to show bigSidebar immediately after a page reloads. Because the initial value is "false" we reverse the logic */}
+      <div className={showSidebar ? 'sidebar-container' : 'sidebar-container show-sidebar'}>
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   )
 }
