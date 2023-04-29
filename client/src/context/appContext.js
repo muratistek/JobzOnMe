5337ddb2cb595f8553ce6e67f712cc37682c4ede
Murat Istek <messi897@gmail.com>
2023-04-29 17:09:00 -0400

    Add complete "update user" front-end func

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -12,6 +12,9 @@ import {
   LOGIN_USER_ERROR,
   TOGGLE_SIDEBAR,
   LOGOUT_USER,
+  UPDATE_USER_BEGIN,
+  UPDATE_USER_SUCCESS,
+  UPDATE_USER_ERROR,
 } from "./actions";
 
 // Fetch data from the local storage on initial load
@@ -145,13 +148,20 @@ const AppProvider = ({ children }) => {
   }
 
   const updateUser = async (currentUser) => {
+    dispatch({ type: UPDATE_USER_BEGIN })
+
     try {
       const { data } = await authFetch.patch('/auth/updateUser', currentUser)
 
-      console.log(data)
+      const { user, location, token } = data
+
+      dispatch({ type: UPDATE_USER_SUCCESS, payload: { user, location, token } })
+      addUserToLocalStorage({ user, location, token })
     } catch (error) {
-      // console.log(error.response)
+      dispatch({ type: UPDATE_USER_ERROR, payload: { msg: error.response.data.msg } })
     }
+
+    clearAlert()
   }
 
   return (
--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -9,6 +9,9 @@ import {
   LOGIN_USER_ERROR,
   TOGGLE_SIDEBAR,
   LOGOUT_USER,
+  UPDATE_USER_BEGIN,
+  UPDATE_USER_SUCCESS,
+  UPDATE_USER_ERROR
 } from "./actions"
 
 import { initialState } from "./appContext"
@@ -96,6 +99,32 @@ const reducer = (state, action) => {
       userLocation: ''
     }
   }
+  if (action.type === UPDATE_USER_BEGIN) {
+    return { ...state, isLoading: true }
+  }
+  if (action.type === UPDATE_USER_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      token: action.payload.token,
+      user: action.payload.user,
+      userLocation: action.payload.location,
+      jobLocation: action.payload.location,
+      showAlert: true,
+      alertType: 'success',
+      alertText: 'User Profile has been updated!',
+    }
+  }
+  if (action.type === UPDATE_USER_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: 'danger',
+      alertText: action.payload.msg,
+    }
+  }
+
 
   // This will run if we dispatch some kind of action that's not handled in our reducer
   throw new Error(`no such action: ${action.type}`)
