f76d420a8b0760d208af3c7870e7770ee3f2b0f0
Murat Istek <messi897@gmail.com>
2023-05-03 19:09:51 -0400

    Add "Search" front-end ui component

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { FormRow, FormRowSelect } from '.'
+import { useAppContext } from '../context/appContext'
+import Wrapper from '../assets/wrappers/SearchContainer'
 
 export default function SearchContainer() {
+  const { isLoading, search, searchStatus, searchType, sort, sortOptions, handleChange, clearFilters, jobTypeOptions, statusOptions } = useAppContext()
+
+  const handleSearch = (e) => {
+    if (isLoading) return
+    handleChange({ name: e.target.name, value: e.target.value })
+  }
+
   return (
-    <h1>Search Component</h1>
+    <Wrapper>
+      <form className='form'>
+        <h4>Search Form</h4>
+        <div className='form-center'>
+          <FormRow type='text' name='search' value={search} handleChange={handleSearch} />
+          {/* add additional inputs */}
+        </div>
+      </form>
+    </Wrapper>
   )
 }
