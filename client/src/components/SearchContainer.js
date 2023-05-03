30cfa8941283285ff52595b8e6b8da34ec381a2d
Murat Istek <messi897@gmail.com>
2023-05-03 19:27:27 -0400

    Add complete "Search Container" ui component

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -11,13 +11,42 @@ export default function SearchContainer() {
     handleChange({ name: e.target.name, value: e.target.value })
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    clearFilters()
+  }
+
   return (
     <Wrapper>
       <form className='form'>
         <h4>Search Form</h4>
         <div className='form-center'>
+          {/* Search by position */}
           <FormRow type='text' name='search' value={search} handleChange={handleSearch} />
-          {/* add additional inputs */}
+          {/* Search by status */}
+          <FormRowSelect
+            labelText="status"
+            name="searchStatus"
+            value={searchStatus}
+            handleChange={handleSearch}
+            list={['all', ...statusOptions]}
+          />
+          {/* Search by a job type  */}
+          <FormRowSelect
+            labelText="Job Type"
+            name="searchType"
+            value={searchType}
+            handleChange={handleSearch}
+            list={['all', ...jobTypeOptions]}
+          />
+          {/* Sort */}
+          <FormRowSelect
+            name="sort"
+            value={sort}
+            handleChange={handleSearch}
+            list={sortOptions}
+          />
+          <button className='btn btn-danger btn-block' disabled={isLoading} onClick={handleSubmit}>clear filters</button>
         </div>
       </form>
     </Wrapper>
