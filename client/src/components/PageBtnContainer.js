a3851657258f8a4cee3a269da2879c1a019cb204
Murat Istek <messi897@gmail.com>
2023-05-04 16:34:52 -0400

    Add icons to page button container

--- client/src/components/PageBtnContainer.js
+++ client/src/components/PageBtnContainer.js
@@ -1,5 +1,29 @@
+import { useAppContext } from "../context/appContext"
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi'
+import Wrapper from "../assets/wrappers/PageBtnContainer"
+
 export default function PageBtnContainer() {
+  const { numOfPages, page } = useAppContext()
+
+  const prevPage = () => {
+    console.log("Previous page")
+  }
+
+  const nextPage = () => {
+    console.log("Next Page")
+  }
+
   return (
-    <div>PageBtnContainer</div>
+    <Wrapper>
+      <button className="prev-btn" onClick={prevPage}>
+        <HiChevronDoubleLeft />
+        prev
+      </button>
+      <div className="btn-container">buttons</div>
+      <button className="next-btn" onClick={nextPage}>
+        next
+        <HiChevronDoubleRight />
+      </button>
+    </Wrapper>
   )
 }
