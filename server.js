1497a882db88cab5e223432383ca268bc5192aa5
Murat Istek <messi897@gmail.com>
2023-05-04 19:21:03 -0400

    Add "express static" middleware
    Add "express static" middleware to handle front end application from the server-side

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ dotenv.config()
 import "express-async-errors"
 import morgan from 'morgan'
 
+// Imports for production
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
 // Database
 import connectDB from './db/connect.js'
 
@@ -26,12 +31,19 @@ if (process.env.NODE_ENV != "production") {
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send("Welcome!")
-})
+// Because we are using ES6 modules, we need to get a proper path with "meta.url"
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// Add the middleware to serve the build folder as a static asset (publicly available) when deploying. This way when we run server, we can return the front-end application
+app.use(express.static(path.resolve(__dirname, './client/build')))
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
+
+// Use this line after the two line above because we want to serve the main index.html after those two routes. This way react router that is located there will get triggered correctly
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
