58b7615329e4082969a51a5545ad7abf6655422c
Murat Istek <messi897@gmail.com>
2023-05-05 14:00:59 -0400

    Add and set "express-rate-limiter" for auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,20 @@
 import express from 'express'
+import rateLimiter from 'express-rate-limit'
+
 const router = express.Router()
+
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000,  // 15 minutes
+  max: 15,  // max 15 requests from a particular ip address in 15 minutes
+  message: 'There are too many requests from this IP address. Please try again in 15 minutes',
+})
 
 import { register, login, updateUser } from "../controllers/authController.js";
 import authenticateUser from '../middleware/auth.js'
 
 // Routes
-router.route('/register').post(register)
-router.route('/login').post(login)
+router.route('/register').post(apiLimiter, register)
+router.route('/login').post(apiLimiter, login)
 
 // Since "register and login" are public routes we shouldn't check if the user is authenticated. But only auth users can update their profile
 router.route('/updateUser').patch(authenticateUser, updateUser)
