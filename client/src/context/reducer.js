712d91f96185faffcfff0e17f7a7e8c4a5e6bb57
Murat Istek <messi897@gmail.com>
2023-05-08 19:27:35 -0400

    Add completed "getCurrentUser" for cookie auth

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -112,7 +112,8 @@ const reducer = (state, action) => {
       ...initialState,
       user: null,
       jobLocation: '',
-      userLocation: ''
+      userLocation: '',
+      userLoading: false
     }
   }
   if (action.type === UPDATE_USER_BEGIN) {
@@ -282,6 +283,18 @@ const reducer = (state, action) => {
     return {
       ...state,
       page: action.payload.page
+    }
+  }
+  if (action.type === GET_CURRENT_USER_BEGIN) {
+    return { ...state, userLoading: true, showAlert: false }
+  }
+  if (action.type === GET_CURRENT_USER_SUCCESS) {
+    return {
+      ...state,
+      userLoading: false,
+      user: action.payload.user,
+      userLocation: action.payload.location,
+      jobLocation: action.payload.location,
     }
   }
 
